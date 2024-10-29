c84b0b4c35ed497b32632425fbe389e285815533
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-10-29 15:01:06 +0200

    1

--- src/app/providers/SessionWrapper.tsx
+++ src/app/providers/SessionWrapper.tsx
@@ -4,5 +4,5 @@ import { SessionProvider } from "next-auth/react";
 import React from "react";
 
 export default function SessionWrapper({ session, children }: { session: any; children: React.ReactNode }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return <SessionProvider >{children}</SessionProvider>;
 }
